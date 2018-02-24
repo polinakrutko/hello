30c40b97375e6ddeefd48c2a5a7ce9f7a377b21c
Polina <polina.krutko@yandex.ru>
2018-02-24 11:34:12 +0000

    Add hello file

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+   string s;
+   cout<<"Pleas, enter your name and surname"<<endl;
+   cin>>s;
+   cout<<"Hello, "<<s<<"!)"<<endl;
+   return 0;
+}
