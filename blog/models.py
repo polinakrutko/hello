d690fa91ecefbf530fc3d53943684f69b398ac0d
Polina Krutko <polina.krutko@yandex.ru>
2018-04-17 07:58:55 +0000

    lenGTH

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    title = models.CharField(max_lenght=200)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
